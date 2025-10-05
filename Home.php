1a8a47dc0ba021273ffe85897b26e064fcc9d6ac
Dikaya134 <akwazahara@gmail.com>
2025-10-05 10:54:16 +0700

    first commit

---
+++ Home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tugas</title>
+</head>
+<body>
+  Masih belajar git 
+  1. Buat folde baru 
+  2. git init 
+  3. git config -global user.name << nama >>
+  4. git config -global user.gmail << gmail >>
+  5. buat file baru
+  6. git add
+  7. git commit -m "Pesan commit"
+  8. buat repository di github
+  9. git remote add origan << url >>
+  10.git push -m main
+</body>
+</html>
